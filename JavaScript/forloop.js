a156deb3209a7012372ffaa7db56b244d63dd005
SanjidaRahmanOmi <sanjidaomi92@gmail.com>
2024-01-09 02:25:58 +0600

    for in loop

--- JavaScript/forloop.js
+++ JavaScript/forloop.js
@@ -1,5 +1,22 @@
+// Normal for loop
 let sum=0;
 for(i=0;i<=10;i++){
     sum=sum+i;
 }
 console.log(sum);
+
+// for in loop
+let obj={
+    name: "Omi",
+    role: "Programmer"
+}
+for (const key in obj){
+        const element = obj[key];
+        console.log(key,element)
+        
+    }
+    /*
+    output:
+    name Omi
+    role Programmer
+    */
