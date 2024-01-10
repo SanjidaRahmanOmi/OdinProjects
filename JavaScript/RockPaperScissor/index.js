0237cba41130e1c7077a2ea819abc9cd1a94c0d7
SanjidaRahmanOmi <sanjidaomi92@gmail.com>
2024-01-10 21:11:40 +0600

    game for one round

--- JavaScript/RockPaperScissor/index.js
+++ JavaScript/RockPaperScissor/index.js
@@ -1,14 +1,29 @@
-function getComputerChoice(){
-const random=Math.floor((Math.random() * 3)+1);
-if(random==1){
-    return "Rock";
-}
-else if(random==2){
-    return "Paper"
-}
-else if(random==3){
-    return "Scissor";
-}
-}
-let computerChoice = getComputerChoice();
-console.log(computerChoice);
+function getComputerChoice() {
+    const random = Math.floor((Math.random() * 3) + 1);
+    if (random === 1) {
+      return "Rock";
+    } else if (random === 2) {
+      return "Paper";
+    } else if (random === 3) {
+      return "Scissor";
+    }
+  }
+
+  function playRound(playerSelection, computerSelection){
+    if(playerSelection===computerSelection){
+      console.log(`It is a tie.`);
+    }
+    else if((playerSelection==="Rock" && computerSelection==="Scissor")||
+    (playerSelection==="Paper" && computerSelection==="Rock")||
+    (playerSelection==="Scissor" && computerSelection==="Paper")){
+      console.log(`You win! ${playerSelection} beats ${computerSelection}`)
+    }
+    else{
+      console.log(`You loss! ${computerSelection} beats ${playerSelection}`)
+    }
+  }
+  const player="rock";
+  const playerSelection = player.toLowerCase();
+  const computerSelection = getComputerChoice().toLowerCase();
+  console.log(computerSelection)
+  playRound(playerSelection, computerSelection);
