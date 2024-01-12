cc5675d4bdbac6c64576332c355dc263f4978cdd
SanjidaRahmanOmi <sanjidaomi92@gmail.com>
2024-01-13 02:20:39 +0600

    Replace and Concat

--- JavaScript/string.js
+++ JavaScript/string.js
@@ -4,3 +4,7 @@ console.log("My name is " +my_name+" " +nickname);
 console.log(my_name.slice(1,4));
 let My_name=my_name.toUpperCase().slice(0,1)+my_name.slice(1); // slice(1) take all the strings and slice(0,1) take letter of index 0 to before 1 that means only 0.
 console.log(My_name);
+console.log(My_name.length);
+console.log(My_name.replace("ji","zi"));
+console.log(my_name.concat(nickname));
+console.log(my_name.concat("Rahman",nickname));
